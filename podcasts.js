c25601407b10c174bcca6606641a66b2c331889b
Jenny Han <jlhan@stanford.edu>
2021-07-06 00:22:05 -0700

    Create podcasts.js

---
+++ podcasts.js
@@ -0,0 +1,15 @@
+let data = {
+        "1": {
+            "title": "perry longs for family",
+            "link": {
+              'url':  'https://open.spotify.com/episode/0MXLCHhLZjlA31du3sMtHw?si=lMrGy8LJT5WmZW8SBRc7QQ&dl_branch=1',
+              "text": "Borders Between Us"
+            },
+            "image": {
+              "alt":"perry with chicken leg",
+              "url":"images/chickn.jpg"
+            },
+            "type":"podcast",
+            "text": "Saidu Tejan-Thomas makes me melt."
+        },
+};
